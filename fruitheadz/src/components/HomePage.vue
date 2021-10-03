63f7ac9e399369c87e694e4a06429d52a74a3f16
Sam Janvey <sjanvey@gmail.com>
2021-10-03 10:31:54 -0400

    moved images to static location

--- fruitheadz/src/components/HomePage.vue
+++ fruitheadz/src/components/HomePage.vue
@@ -2,8 +2,8 @@
   <div class="container-fluid text-center">
     <h1 class="mb-4">FruitHeadz</h1>
     <div class="fruitImages">
-      <img class="img-fluid m-1" src="@/assets/images/fh1.png" alt="FruitHead1"/>
-      <img class="img-fluid m-1" src="@/assets/images/fh2.png" alt="FruitHead2"/>
+      <img class="img-fluid m-1" src="/images/fh1.png" alt="FruitHead1"/>
+      <img class="img-fluid m-1" src="/images/fh2.png" alt="FruitHead2"/>
     </div>
     <button type="button" class="btn btn-secondary btn-lg mt-5" disabled>Minting Soon!</button>
   </div>
