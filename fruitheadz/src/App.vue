66407bf8d24cd785a00dd64b0098e840fb72c23a
Sam Janvey <sjanvey@gmail.com>
2021-10-03 09:14:23 -0400

    FruitHeadz init

---
+++ fruitheadz/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <home-page></home-page>
+  </div>
+</template>
+
+<script>
+import HomePage from '@/components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #000000;
+  color: #ffffff;
+}
+
+</style>
---
+++ fruitheadz/src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome to FruitHeadz</h1>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomePage',
+  data(){
+    return{}
+  },
+  methods:{
+  }
+}
+</script>
+
+<style scoped>
+</style>
