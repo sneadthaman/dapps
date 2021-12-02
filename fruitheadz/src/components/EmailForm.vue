9ffd14b245545cb58d05c5c98fd57734dc07ccfa
Sam Janvey <sjanvey@gmail.com>
2021-12-02 11:39:43 -0500

    Tutorial and FruitHeadz files, React and Vue

--- fruitheadz/src/components/EmailForm.vue
+++ fruitheadz/src/components/EmailForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mt-5 text-center" v-if="show">
       <h2>Stay Up To Date</h2>
-      <form name="email" class="p-3" netlify>
+      <form name="email" class="p-3" v-on:submit.prevent="addEmail">
         <div class="form-group">
           <label for="email">Email Address</label>
           <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required v-model="email">
@@ -13,6 +13,47 @@
 </template>
 
 <script>
+
+// Import the functions you need from the SDKs you need
+
+import Firebase from 'firebase/app'
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyA5jF1FTiKhl7rnhoqVEpqJ82k0TtVB3bk",
+
+  authDomain: "fruitheadz-855a3.firebaseapp.com",
+
+  databaseURL: "https://fruitheadz-855a3-default-rtdb.firebaseio.com",
+
+  projectId: "fruitheadz-855a3",
+
+  storageBucket: "fruitheadz-855a3.appspot.com",
+
+  messagingSenderId: "561761387591",
+
+  appId: "1:561761387591:web:c78428b8a066bc9ad19d62",
+
+  measurementId: "G-BBNZ2R94J9"
+
+};
+
+
+// Initialize Firebase
+
+let app = Firebase.initializeApp(firebaseConfig)
+let db = app.database()
+let emailRef = db.ref('email')
+
 export default{
   name: 'email-form',
   data(){
@@ -22,6 +63,10 @@ export default{
     }
   },
   methods:{
+    addEmail() {
+      emailRef.push(this.email)
+      this.email = ''
+    }
   }
 }
 </script>
---
+++ react-apps/nader-tutorial/hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/4d5df27acce645e3945e4371be8ee748",
+      accounts: [`0x8e290362bb93d3b5c28dceb1efa409f88262b6002728d6bb774fd2e16449e349`]
+    }
+  }
+};
